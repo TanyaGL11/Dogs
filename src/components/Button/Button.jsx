067686925c25b4f8152ba28d81f075f512ea9c8b
unknown <tanya11051978@mail.ru>
2022-12-13 16:19:29 +0300

    button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,12 +3,12 @@ import s from './style.module.css'
 function Button({ text, type }) {
   const onClickBtn = () => console.log('CLICK')
   return (
-    // eslint-disable-next-line react/button-has-type, quotes
-    <button
-      className={s.btn, {
+   <button
+      className={s.btn}
+      /*{s.btn, {
         [s.secondary]: type === 'secondary',
         [s.primary]: type === 'primary',
-      }}
+      }}*/
       onClick={onClickBtn}
     >
       { text }
